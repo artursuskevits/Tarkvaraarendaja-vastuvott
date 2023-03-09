c541b656695ab136b9edcb8a2cc42697d433fc7b
artursuskevits@gmail.com <artursuskevits@gmail.com>
2023-03-08 20:39:21 -0800

    ^_^

--- Tarkvaraarendaja vastuvott/Tarkvaraarendaja_vastuvott.py
+++ Tarkvaraarendaja vastuvott/Tarkvaraarendaja_vastuvott.py
@@ -8,18 +8,9 @@ koik = loe_faelist(koik,"koik.txt")
 print(koik)
 while True:
     try:
-        #file=open("vastus.txt", "r", encoding="utf-8-sig")
-        #for line in file:
-        #    k, v=line.strip().split("-")
-        #    sonastik[k.strip()] = v.strip()
-        #    file=open("vastus.txt", "r", encoding="utf-8-sig")
-        #for line in file:
-        #    k, v=line.strip().split("-")
-        #    sonastik2[v.strip()] = k.strip()
-        #    file=open("vastus.txt", "r", encoding="utf-8-sig")
         txttodictionary(sonastik,sonastik2,"vastus.txt")
         nimi=input("kirjuta siinu eesnimi ja perenimi ")
-        menu=int(input(f"{nimi} mida te tahate, \n1-test \n2-valja"))
+        menu=int(input(f"{nimi} te soovite testi teha, \n1-yah \n2-ei"))
         if menu==1:
             koik=test(sonastik,sonastik2,nimi,koik)
             sorter(koik,vastuvoetud,eisoobi,"koik.txt","vastuvoetud.txt","eisoobi.txt")
